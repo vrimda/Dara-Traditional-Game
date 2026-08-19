e4d98689bc94efac2730aef18405a2c1608b8926
vrimda <81833497+vrimda@users.noreply.github.com>
2026-08-20 00:55:34 +0100

    Change base path in Vite configuration
    Updated base path for deployment and removed build/server options.

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    minify: true,
-  },
-  server: {
-    port: 3000,
-  },
+  base: '/Dara-Traditional-Game/'  // <-- this line is critical
 })
